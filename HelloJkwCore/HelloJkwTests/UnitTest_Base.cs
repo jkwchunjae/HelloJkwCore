a78319688c585ca617f1b5ced9b98a04a91a6f60
jkw <jkwchunjae@daum.net>
2020-05-04 02:01:33 +0900

    Init unit test (diary)

---
+++ HelloJkwCore/HelloJkwTests/UnitTest_Base.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HelloJkwTests
+{
+    static class UnitTest_Base
+    {
+        private static readonly string ConfigurationRootPath;
+
+        static UnitTest_Base()
+        {
+            // D:\jkw\major\GitHub\HelloJkwCore\HelloJkwCore\HelloJkwTests\bin\Debug\netcoreapp3.1
+
+            var currentDirInfo = new DirectoryInfo(Environment.CurrentDirectory);
+            ConfigurationRootPath = Path.Combine(currentDirInfo.Parent.Parent.Parent.Parent.FullName, "HelloJkwServer");
+        }
+
+        public static IConfiguration GetIConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(ConfigurationRootPath)
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
