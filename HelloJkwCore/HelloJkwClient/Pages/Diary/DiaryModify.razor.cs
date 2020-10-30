cce42c6874c0af22f59d98a908e10340a759cde1
jkw <jkwchunjae@daum.net>
2020-10-30 23:10:45 +0900

    razor, cs 분리

---
+++ HelloJkwCore/HelloJkwClient/Pages/Diary/DiaryModify.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloJkwClient.Pages.Diary
+{
+    public partial class DiaryModify : ComponentBase
+    {
+        [Parameter]
+        public string DiaryName { get; set; }
+        [Parameter]
+        public string DateStr { get; set; }
+
+
+    }
+}
---
+++ HelloJkwCore/HelloJkwClient/Pages/Diary/DiaryWrite.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloJkwClient.Pages.Diary
+{
+    public partial class DiaryWrite : ComponentBase
+    {
+        [Parameter]
+        public string DiaryName { get; set; }
+
+    }
+}
