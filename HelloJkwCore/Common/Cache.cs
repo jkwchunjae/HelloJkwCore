3f0c6d5144a857cf243ff485cee10d79c2ec1bc2
jkw <jkwchunjae@daum.net>
2020-11-17 00:12:20 +0900

    Cache에 ttl 추가

--- HelloJkwCore/Common/Cache.cs
+++ HelloJkwCore/Common/Cache.cs
@@ -4,25 +4,44 @@ using System.Text;
 
 namespace Common
 {
+    public class CacheData<T>
+    {
+        public T Data { get; set; }
+        public DateTime? ExpiredAt { get; set; }
+    }
+
     public class MemoryCache<T> : ICache<T>
     {
-        private Dictionary<string, T> _cached;
+        private Dictionary<string, CacheData<T>> _cached;
 
         public MemoryCache()
         {
-            _cached = new Dictionary<string, T>();
+            _cached = new Dictionary<string, CacheData<T>>();
         }
 
         public bool Contains(string key)
-            => _cached.ContainsKey(key);
-
-        public void Set(string key, T value)
         {
-            _cached[key] = value;
+            if (_cached.TryGetValue(key, out var data))
+            {
+                if (data.ExpiredAt == null)
+                    return true;
+                if (data.ExpiredAt > DateTime.Now)
+                    return true;
+            }
+            return false;
+        }
+
+        public void Set(string key, T value, TimeSpan? ttl = null)
+        {
+            _cached[key] = new CacheData<T>
+            {
+                Data = value,
+                ExpiredAt = ttl.HasValue ? DateTime.Now + ttl.Value : (DateTime?)null,
+            };
         }
 
         public T Get(string key)
-            => Contains(key) ? _cached[key] : default;
+            => Contains(key) ? _cached[key].Data : default;
 
         public bool TryGet(string key, out T value)
         {
