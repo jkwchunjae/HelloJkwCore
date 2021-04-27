eb4b6cc79a45d1087502c3cf0271ee2dc90f87a7
jkw <jkwchunjae@daum.net>
2021-04-28 00:44:45 +0900

    System.Text.Json 제거

--- HelloJkwCore/Common/Authentication/AuthProvider.cs
+++ HelloJkwCore/Common/Authentication/AuthProvider.cs
@@ -1,9 +1,7 @@
 ﻿using Newtonsoft.Json.Converters;
-using System.Text.Json.Serialization;
 
 namespace Common
 {
-    [System.Text.Json.Serialization.JsonConverter(typeof(JsonStringEnumConverter))]
     [Newtonsoft.Json.JsonConverter(typeof(StringEnumConverter))]
     public enum AuthProvider
     {
--- HelloJkwCore/Common/FileSystem/FileSystemType.cs
+++ HelloJkwCore/Common/FileSystem/FileSystemType.cs
@@ -3,12 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Common
 {
-    [System.Text.Json.Serialization.JsonConverter(typeof(JsonStringEnumConverter))]
     [Newtonsoft.Json.JsonConverter(typeof(StringEnumConverter))]
     public enum FileSystemType
     {
