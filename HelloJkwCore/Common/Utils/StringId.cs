03435ae0b726eba0010ec4f5a32c58cc51ee7e70
jkw <jkwchunjae@daum.net>
2021-04-28 10:08:36 +0900

    StringId == overloading

--- HelloJkwCore/Common/Utils/StringId.cs
+++ HelloJkwCore/Common/Utils/StringId.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Common
 {
-    public class StringId : IComparable<StringId>, IComparable<string>
+    public class StringId : IComparable<StringId>, IEquatable<StringId>
     {
         public string Id { get; set; }
 
@@ -21,15 +21,51 @@ namespace Common
         {
             return Id;
         }
-
         public int CompareTo(StringId other)
         {
             return Id.CompareTo(other.Id);
         }
-
-        public int CompareTo(string other)
+        public static bool operator ==(StringId obj1, StringId obj2)
         {
-            return Id.CompareTo(other);
+            if (ReferenceEquals(obj1, obj2))
+            {
+                return true;
+            }
+            if (ReferenceEquals(obj1, null))
+            {
+                return false;
+            }
+            if (ReferenceEquals(obj2, null))
+            {
+                return false;
+            }
+
+            return obj1.Equals(obj2);
+        }
+        public static bool operator !=(StringId obj1, StringId obj2)
+        {
+            return !(obj1 == obj2);
+        }
+        public bool Equals(StringId other)
+        {
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Id == other.Id;
+        }
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as StringId);
+        }
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
         }
     }
 
