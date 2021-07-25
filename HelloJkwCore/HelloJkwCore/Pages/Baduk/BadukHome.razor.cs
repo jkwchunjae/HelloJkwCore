531182112e7cd072d39cbbef327b6b57557cd8c2
jkw <jkwchunjae@daum.net>
2021-07-25 14:45:31 +0900

    Front에서 쓰는 변수 위치 변경

--- HelloJkwCore/HelloJkwCore/Pages/Baduk/BadukHome.razor.cs
+++ HelloJkwCore/HelloJkwCore/Pages/Baduk/BadukHome.razor.cs
@@ -12,20 +12,7 @@ namespace HelloJkwCore.Pages.Baduk
 {
     public partial class BadukHome : JkwPageBase
     {
-        [Inject]
-        private IJSRuntime JsRuntime { get; set; }
-
-        private int Size => Board?.Size ?? 19;
-
-        private Dictionary<int, List<int>> PointArray = new()
-        {
-            [19] = new() { 4, 10, 16 },
-            [15] = new() { 4, 8, 12 },
-            [13] = new() { 4, 7, 10 },
-            [9] = new() { 5 },
-        };
         private BadukBoard Board { get; set; } = new BadukBoard(19);
-        private string IndexText => $"{Board.CurrentIndex} / {Board.LastIndex}";
 
         protected override Task OnPageInitializedAsync()
         {
@@ -34,7 +21,6 @@ namespace HelloJkwCore.Pages.Baduk
 
         private void ClickCell(int row, int column)
         {
-            JsRuntime.ConsoleLogAsync("log", Board, row, column);
             Board.ClickCell(row, column);
             StateHasChanged();
         }
