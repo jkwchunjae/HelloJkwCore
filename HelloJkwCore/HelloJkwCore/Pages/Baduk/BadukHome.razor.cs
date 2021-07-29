7e252590b42ba79e3ef2f3ae9aabe2c978d64c07
jkw <jkwchunjae@daum.net>
2021-07-29 22:49:49 +0900

    바둑 삭제 Component

--- HelloJkwCore/HelloJkwCore/Pages/Baduk/BadukHome.razor.cs
+++ HelloJkwCore/HelloJkwCore/Pages/Baduk/BadukHome.razor.cs
@@ -17,100 +17,32 @@ namespace HelloJkwCore.Pages.Baduk
         private IBadukService BadukService { get; set; }
         private BadukBoard Board { get; set; } = new BadukBoard(19);
 
-        private BadukDiary Diary = null;
         private List<BadukDiary> DiaryList = new();
-        private List<BadukGameData> GameDataList = new();
-
-        private string SaveFileName = string.Empty;
-        private bool DeleteFlag = false;
-
-        private string NewDiaryName = string.Empty;
-        private bool DiaryDeleteFlag = false;
-
-        protected override async Task OnPageInitializedAsync()
-        {
-            if (IsAuthenticated)
-            {
-                await Init();
-            }
-        }
-
-        private async Task Init()
-        {
-            DiaryList = await BadukService.GetBadukDiaryList(User);
-            if (DiaryList.Any())
-            {
-                Diary = DiaryList.First();
-                GameDataList = await BadukService.GetBadukSummaryList(Diary.Name);
-            }
-        }
 
         private void ChangeSize(int size)
         {
             Board = new BadukBoard(size);
-            SaveFileName = string.Empty;
-            DeleteFlag = false;
         }
 
-        private async Task SaveBoard()
+        private ValueTask OnGameDataSaved(BadukGameData saveGameData)
         {
-            if (SaveFileName.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
-            {
-                return;
-            }
-            if (Diary is null)
-            {
-                return;
-            }
-
-            var prevGameData = GameDataList.Find(x => x.Subject == SaveFileName);
-
-            var gameData = new BadukGameData
-            {
-                Subject = SaveFileName,
-                Favorite = prevGameData?.Favorite ?? false,
-                CreateTime = prevGameData?.CreateTime ?? DateTime.Now,
-                LastModifyTime = DateTime.Now,
-                OwnerEmail = User.Email,
-                Size = Board.Size,
-                ChangeMode = Board.ChangeMode,
-                CurrentColor = Board.CurrentColor,
-                CurrentIndex = Board.CurrentIndex,
-                VisibleStoneIndex = Board.VisibleStoneIndex,
-                Memo = prevGameData?.Memo ?? string.Empty,
-                StoneLog = Board.StoneLog,
-            };
-
-            await BadukService.SaveBadukGameData(Diary.Name, gameData);
-            GameDataList = await BadukService.GetBadukSummaryList(Diary.Name);
-            DeleteFlag = false;
+            return ValueTask.CompletedTask;
         }
 
-        private async Task DeleteBoard()
+        private ValueTask OnGameDataDeleted()
         {
-            if (GameDataList.Any(x => x.Subject == SaveFileName))
-            {
-                await BadukService.DeleteBadukGameData(Diary.Name, SaveFileName);
-                GameDataList = await BadukService.GetBadukSummaryList(Diary.Name);
-                Board = new BadukBoard(Board.Size);
-                SaveFileName = string.Empty;
-                DeleteFlag = false;
-            }
+            Board = new BadukBoard(Board.Size);
+            return ValueTask.CompletedTask;
         }
 
-        private void ChangeGameData(ChangeEventArgs args)
+        private void ChangeGameData(BadukGameData gameData)
         {
-            if (string.IsNullOrEmpty((string)args.Value))
+            if (gameData is null)
             {
                 Board = new BadukBoard(Board.Size);
-                SaveFileName = string.Empty;
-                DeleteFlag = false;
             }
-            else if (int.TryParse((string)args.Value, out var index))
+            else
             {
-                var gameData = GameDataList[index];
-                JsRuntime.ConsoleLogAsync("log", gameData);
-
                 Board = new BadukBoard(gameData.Size, gameData.StoneLog)
                 {
                     CurrentColor = gameData.CurrentColor,
@@ -118,45 +50,6 @@ namespace HelloJkwCore.Pages.Baduk
                     CurrentIndex = gameData.CurrentIndex,
                     VisibleStoneIndex = gameData.VisibleStoneIndex,
                 };
-
-                SaveFileName = gameData.Subject;
-                DeleteFlag = false;
-            }
-        }
-
-        private async Task CreateDiary()
-        {
-            if (string.IsNullOrEmpty(NewDiaryName.Trim()))
-                return;
-
-            if (NewDiaryName.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
-                return;
-
-            await BadukService.CreateBadukDiary(User, new DiaryName(NewDiaryName));
-            await Init();
-        }
-
-        private async Task DeleteDiary()
-        {
-            if (string.IsNullOrEmpty(NewDiaryName.Trim()))
-                return;
-
-            var diaryName = new DiaryName(NewDiaryName.Trim());
-            if (DiaryList.Any(x => x.Name == diaryName))
-            {
-                await BadukService.DeleteBadukDiary(User, diaryName);
-            }
-        }
-
-        private async Task ChangeDiaryInfo(ChangeEventArgs args)
-        {
-            if (int.TryParse((string)args.Value, out var index))
-            {
-                Diary = DiaryList[index];
-                GameDataList = await BadukService.GetBadukSummaryList(Diary.Name);
-                Board = new BadukBoard(Board.Size);
-                SaveFileName = string.Empty;
-                DeleteFlag = false;
             }
         }
     }
