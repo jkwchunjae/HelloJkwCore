737151b3908730be895d230cb6ccaf31347c5610
jkw <jkwchunjae@daum.net>
2021-11-06 21:15:26 +0900

    Revert "Delete DiaryCreate"
    This reverts commit 215828d5770925d3fbf7d06a41aef902f256ee1d.

---
+++ HelloJkwCore/HelloJkwCore/Pages/Diary/DiaryCreate.razor.cs
@@ -0,0 +1,37 @@
+﻿using HelloJkwCore.Shared;
+using Microsoft.AspNetCore.Components;
+using ProjectDiary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HelloJkwCore.Pages.Diary
+{
+    public partial class DiaryCreate : JkwPageBase
+    {
+        [Inject]
+        IDiaryService DiaryService { get; set; }
+
+        DiaryCreateModel _createModel = new DiaryCreateModel();
+
+        private async Task CreateDiaryAsync(DiaryCreateModel model)
+        {
+            try
+            {
+                var diary = await DiaryService.CreateDiaryInfoAsync(User, model.DiaryName, model.IsSecret);
+                Navi.NavigateTo(DiaryUrl.Home(diary.DiaryName));
+            }
+            catch
+            {
+            }
+        }
+    }
+
+    public class DiaryCreateModel
+    {
+        public string DiaryName { get; set; }
+        public bool IsSecret { get; set; }
+    }
+}
