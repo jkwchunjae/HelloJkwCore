960cef6712f946fbc8135445dcfd422eeed4648c
jkw <jkwchunjae@daum.net>
2021-11-07 19:12:24 +0900

    Revert "(임시) Baduk 삭제"
    This reverts commit 3a711db76deb51fae390ae825474c93d1b49c59d.

---
+++ HelloJkwCore/HelloJkwCore/Pages/Baduk/BadukHome.razor.cs
@@ -0,0 +1,47 @@
+﻿using Common;
+using HelloJkwCore.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using ProjectBaduk;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloJkwCore.Pages.Baduk
+{
+    public partial class BadukHome : JkwPageBase
+    {
+        private BadukBoard Board { get; set; } = new BadukBoard(19);
+
+        private ValueTask OnGameDataSaved(BadukGameData saveGameData)
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        private ValueTask OnGameDataDeleted()
+        {
+            Board = new BadukBoard(Board.Size);
+            return ValueTask.CompletedTask;
+        }
+
+        private void ChangeGameData(BadukGameData gameData)
+        {
+            if (gameData is null)
+            {
+                Board = new BadukBoard(Board.Size);
+            }
+            else
+            {
+                Board = new BadukBoard(gameData.Size, gameData.StoneLog)
+                {
+                    CurrentColor = gameData.CurrentColor,
+                    ChangeMode = gameData.ChangeMode,
+                    CurrentIndex = gameData.CurrentIndex,
+                    VisibleStoneIndex = gameData.VisibleStoneIndex,
+                };
+            }
+        }
+    }
+}
