0f97889faf0efb1f533259635e963a85e858572e
jkw <jkwchunjae@daum.net>
2021-11-07 15:33:25 +0900

    Update DiarySearch

--- HelloJkwCore/ProjectDiary/Search/DiarySearchData.cs
+++ HelloJkwCore/ProjectDiary/Search/DiarySearchData.cs
@@ -8,8 +8,8 @@ namespace ProjectDiary
 {
     public class DiarySearchData
     {
-        public DateTime BeginDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime? BeginDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public string DayOfWeek { get; set; }
         public string Keyword { get; set; }
     }
