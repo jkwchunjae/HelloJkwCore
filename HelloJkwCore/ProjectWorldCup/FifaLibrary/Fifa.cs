9136ecc39228fffd3d4856070c714d8dcf0298f3
jkw <jkwchunjae@daum.net>
2021-12-23 01:16:14 +0900

    FIFA api

---
+++ HelloJkwCore/ProjectWorldCup/FifaLibrary/Fifa.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProjectWorldCup
+{
+    public partial class Fifa : IFifa
+    {
+        private static HttpClient _httpClient = new();
+
+        private async Task<JObject?> GetPageData(Uri uri)
+        {
+            var res = await _httpClient.GetAsync(uri);
+
+            var text = await res.Content.ReadAsStringAsync();
+
+            var pattern = @"<script id=""__NEXT_DATA__"".*?>(.*?)<\/script>";
+
+            var match = Regex.Match(text, pattern);
+
+            var data = match.Groups[1].Captures[0].Value;
+
+            var obj = JObject.Parse(data);
+
+            var o = obj["props"]?["pageProps"]?["pageData"];
+
+            return o?.ToObject<JObject>() ?? null;
+        }
+    }
+}
---
+++ HelloJkwCore/ProjectWorldCup/FifaLibrary/Fifa_QualifiedTeams.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProjectWorldCup
+{
+    public class TeamFlag
+    {
+        public string Src { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Title { get; set; }
+        public string Alt { get; set; }
+    }
+    public class QualifiedTeam
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public TeamFlag Flag { get; set; }
+        public string Status { get; set; }
+    }
+
+    public partial class Fifa : IFifa
+    {
+        public async Task<List<QualifiedTeam>> GetQualifiedTeamsAsync()
+        {
+            try
+            {
+                var url = @"https://www.fifa.com/tournaments/mens/worldcup/qatar2022/qualifiers";
+
+                var pageData = await GetPageData(new Uri(url));
+
+                var list = pageData?["tournamentQualificationTeamsProps"]?["teams"]?.AsEnumerable() ?? null;
+
+                var result = list?
+                    .Select(x => x.ToObject<QualifiedTeam>())
+                    .Where(x => x != null)
+                    .Where(x => x?.Status == "Q")
+                    .ToList();
+
+                return result ?? new();
+            }
+            catch
+            {
+                return new();
+            }
+        }
+    }
+}
