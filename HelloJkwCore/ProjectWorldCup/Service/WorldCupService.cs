f2fba5395e8e037036f4e0eb7cdb2705d707a950
jkw <jkwchunjae@daum.net>
2021-12-23 01:16:34 +0900

    WorldCup service

---
+++ HelloJkwCore/ProjectWorldCup/Service/WorldCupService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWorldCup
+{
+    public class WorldCupService : IWorldCupService
+    {
+        private IFifa _fifa;
+
+        public WorldCupService(IFifa fifa)
+        {
+            _fifa = fifa;
+        }
+
+        public Task<List<QualifiedTeam>> Get2022QualifiedTeamsAsync()
+        {
+            return _fifa.GetQualifiedTeamsAsync();
+        }
+    }
+}
