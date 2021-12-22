7b8cc1306a11c96050cb6677dd1e4588a50e738b
jkw <jkwchunjae@daum.net>
2021-12-23 01:17:02 +0900

    홈페이지에 worldcup 메뉴 추가, 진출확정국가 표시 초안

---
+++ HelloJkwCore/HelloJkwCore/Pages/WorldCup/QualifiedTeamsPage.razor.cs
@@ -0,0 +1,21 @@
+﻿using HelloJkwCore.Shared;
+using Microsoft.AspNetCore.Components;
+using ProjectWorldCup;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HelloJkwCore.Pages.WorldCup
+{
+    public partial class QualifiedTeamsPage : JkwPageBase
+    {
+        [Inject]
+        private IWorldCupService Service { get; set; }
+
+        private List<QualifiedTeam> QualifiedTeams = new();
+
+        protected override async Task OnPageInitializedAsync()
+        {
+            QualifiedTeams = await Service.Get2022QualifiedTeamsAsync();
+        }
+    }
+}
