9840367ed37592593927c772dfa3208345e25317
jkw <jkwchunjae@daum.net>
2021-12-31 01:09:53 +0900

    닉네임 기능 추가

--- HelloJkwCore/Common/User/AppUser.cs
+++ HelloJkwCore/Common/User/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,8 @@ namespace Common
         public string Id { get; set; }
         public DateTime CreateTime { get; set; }
         public string UserName { get; set; }
+        public string NickName { get; set; }
+        [JsonIgnore] public string DisplayName => !string.IsNullOrEmpty(NickName) ? NickName : UserName;
         public string Email { get; set; }
         public ThemeType Theme { get; set; }
         public List<UserRole> Roles { get; set; }
---
+++ HelloJkwCore/HelloJkwCore/Pages/Users/UserPage.razor.cs
@@ -0,0 +1,55 @@
+﻿using Common;
+using HelloJkwCore.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace HelloJkwCore.Pages.Users
+{
+    public partial class UserPage : JkwPageBase
+    {
+        private bool ChangeNickName = false;
+
+        private string InputNickName = string.Empty;
+
+        protected override void OnPageInitialized()
+        {
+            CheckPageChangeNickName(Navi.Uri);
+        }
+
+        protected override Task HandleLocationChanged(LocationChangedEventArgs e)
+        {
+            CheckPageChangeNickName(e.Location);
+
+            return Task.CompletedTask;
+        }
+
+        private void CheckPageChangeNickName(string url)
+        {
+            if (url.Contains("change-nickname"))
+            {
+                ChangeNickName = true;
+                InputNickName = User?.DisplayName;
+            }
+            else
+            {
+                ChangeNickName = false;
+            }
+
+            StateHasChanged();
+        }
+
+        private async Task ChangeNickNameAsync()
+        {
+            User.NickName = InputNickName?.Trim();
+            if (string.IsNullOrWhiteSpace(User.NickName))
+            {
+                User.NickName = null;
+            }
+            await UserStore.UpdateAsync(User, default);
+
+            Navi.NavigateTo("/user");
+        }
+    }
+}
