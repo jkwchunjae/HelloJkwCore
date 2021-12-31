2e9a08097cd905c7e28d73f126b82232fe2b8b98
jkw <jkwchunjae@daum.net>
2021-12-31 23:23:36 +0900

    매치의 승무패 테스트

--- HelloJkwCore/ProjectWorldCup/Models/Match.cs
+++ HelloJkwCore/ProjectWorldCup/Models/Match.cs
@@ -14,5 +14,13 @@ namespace ProjectWorldCup
         public int AwayScore { get; set; }
         public int HomePenaltyScore { get; set; }
         public int AwayPenaltyScore { get; set; }
+
+        public bool IsDraw => HomeScore == AwayScore && HomePenaltyScore == AwayPenaltyScore;
+        public (Team Team, int Score, int PenaltyScore) Winner => HomeScore != AwayScore
+            ? (HomeScore > AwayScore ? (HomeTeam, HomeScore, HomePenaltyScore) : (AwayTeam, AwayScore, AwayPenaltyScore))
+            : (HomePenaltyScore > AwayPenaltyScore ? (HomeTeam, HomeScore, HomePenaltyScore) : (AwayTeam, AwayScore, AwayPenaltyScore));
+        public (Team Team, int Score, int PenaltyScore) Looser => HomeScore != AwayScore
+            ? (HomeScore < AwayScore ? (HomeTeam, HomeScore, HomePenaltyScore) : (AwayTeam, AwayScore, AwayPenaltyScore))
+            : (HomePenaltyScore < AwayPenaltyScore ? (HomeTeam, HomeScore, HomePenaltyScore) : (AwayTeam, AwayScore, AwayPenaltyScore));
     }
 }
