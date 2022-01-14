779afc3abea6d2a66d1e8fd468e49de7e0b6933b
jkw <jkwchunjae@daum.net>
2022-01-15 00:34:13 +0900

    특정 숫자에서 반올림 하는 기능

--- HelloJkwCore/Common/Utils/CommonExtensions.cs
+++ HelloJkwCore/Common/Utils/CommonExtensions.cs
@@ -12,5 +12,21 @@ namespace Common
         {
             return value >= min && value < max;
         }
+
+        public static int RoundN(this int value, int digits, int N)
+        {
+            if (N <= 0 || N > 9)
+                throw new ArgumentException("N은 1부터 9사이 값이어야합니다.");
+
+            var result = value;
+            var tens = (int)Math.Pow(10, -digits);
+            result /= tens;
+
+            var up = (value / (tens / 10)) % 10 >= N; // digits에 해당하는 숫자가 N 이상인지? N이상이면 올림한다.
+            result += (up ? 1 : 0);
+            result  *= tens;
+
+            return result;
+        }
     }
 }
