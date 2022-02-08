072e346ea21284d58dc7436279befb992232fd36
jkw <jkwchunjae@daum.net>
2022-02-08 23:59:58 +0900

    Test betting table 국기 추가

--- HelloJkwCore/ProjectWorldCup/Pages/Betting/BettingResultComponent.razor.cs
+++ HelloJkwCore/ProjectWorldCup/Pages/Betting/BettingResultComponent.razor.cs
@@ -2,44 +2,58 @@
 
 namespace ProjectWorldCup.Pages.Betting;
 
-public class CommonBettingItem : BettingResultItem
+public class WcBettingItem : IBettingResultItem
 {
     public AppUser User { get; init; }
+    public List<Team> Picked { get; set; }
+    public List<Team> Fixed { get; set; }
+    public int Reward { get; set; }
+
+    public string Id
+    {
+        get => User.Id.ToString();
+        set { }
+    }
+    public int Score
+    {
+        get => Success.Count;
+        set { }
+    }
+    public List<Team> Success => Picked?.Where(s => Fixed?.Contains(s) ?? false).ToList() ?? new();
+    public List<Team> Fail => Picked?.Where(s => !(Fixed?.Contains(s) ?? false)).ToList() ?? new();
 }
 
 public partial class BettingResultComponent : JkwPageBase
 {
     [Inject]
     UserManager<AppUser> UserManager { get; set; }
-    BettingResultTable<CommonBettingItem> BettingResult { get; set; }
+    [Inject] 
+    IWorldCupService WorldCupService { get; set; }
 
+    BettingResultTable<WcBettingItem> BettingResult { get; set; }
 
     public BettingResultComponent()
     {
-        BettingResult = new BettingResultTable<CommonBettingItem>(new List<CommonBettingItem>());
+        BettingResult = new BettingResultTable<WcBettingItem>(new List<WcBettingItem>());
     }
 
     protected override async Task OnPageInitializedAsync()
     {
-        var users = (await UserManager.GetUsersInRoleAsync("all")).ToList();
+        var users = await UserManager.GetUsersInRoleAsync("all");
+        var teams = await WorldCupService.Get2022QualifiedTeamsAsync();
+        var @fixed = teams.RandomShuffle().Take(7).ToList();
 
-        var list = new List<CommonBettingItem>();
-        var scores = new[] { 3, 7, 1, 2, 8 };
-        var index = 0;
-        foreach (var user in users.Concat(users))
-        {
-            if (user != null)
+        var list = users.Concat(users)
+            .Where(user => user != null)
+            .Select(user => new WcBettingItem
             {
-                list.Add(new CommonBettingItem
-                {
-                    User = user,
-                    Id = User.Id.ToString(),
-                    Score = scores[index % scores.Length],
-                });
-            }
-            index++;
-        }
+                User = user,
+                Id = User.Id.ToString(),
+                Picked = teams.RandomShuffle().Take(7).ToList(),
+                Fixed = @fixed,
+            })
+            .ToList();
 
-        BettingResult = new BettingResultTable<CommonBettingItem>(list);
+        BettingResult = new BettingResultTable<WcBettingItem>(list);
     }
 }
