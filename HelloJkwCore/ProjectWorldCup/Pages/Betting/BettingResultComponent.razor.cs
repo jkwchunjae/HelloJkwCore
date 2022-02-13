42bc7e4ade3127359a384cda741395288180de33
Kyungwon Jeon <jkwchunjae@daum.net>
2022-02-13 23:56:57 +0900

    Betting table (#43)
    * vs task
    
    * Test betting table
    
    * Test betting table 국기 추가
    
    * WcBettingItem 위치 이동
    
    * 테스트 페이지 수정

---
+++ HelloJkwCore/ProjectWorldCup/Pages/Betting/BettingResultComponent.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ProjectWorldCup.Pages.Betting;
+
+public partial class BettingResultComponent : JkwPageBase
+{
+    [Inject]
+    UserManager<AppUser> UserManager { get; set; }
+    [Inject] 
+    IWorldCupService WorldCupService { get; set; }
+
+    BettingResultTable<WcBettingItem> BettingResult { get; set; }
+
+    public BettingResultComponent()
+    {
+        BettingResult = new BettingResultTable<WcBettingItem>(new List<WcBettingItem>());
+    }
+
+    protected override async Task OnPageInitializedAsync()
+    {
+        var users = await UserManager.GetUsersInRoleAsync("all");
+        var teams = await WorldCupService.Get2022QualifiedTeamsAsync();
+        var @fixed = teams.RandomShuffle().Take(7).ToList();
+
+        var list = users.Concat(users)
+            .Where(user => user != null)
+            .Select(user => new WcBettingItem
+            {
+                User = user,
+                Id = User.Id.ToString(),
+                Picked = teams.RandomShuffle().Take(7).ToList(),
+                Fixed = @fixed,
+            })
+            .ToList();
+
+        BettingResult = new BettingResultTable<WcBettingItem>(list);
+    }
+}
