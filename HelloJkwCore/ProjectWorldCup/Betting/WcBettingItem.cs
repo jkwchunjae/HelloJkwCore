a5c6a0f017c9c12c6e3fe4b297ea11128edcd58c
jkw <jkwchunjae@daum.net>
2022-02-13 23:51:45 +0900

    WcBettingItem 위치 이동

---
+++ HelloJkwCore/ProjectWorldCup/Betting/WcBettingItem.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjectWorldCup;
+
+public class WcBettingItem : IBettingResultItem
+{
+    public AppUser User { get; init; }
+    public List<Team> Picked { get; set; }
+    public List<Team> Fixed { get; set; }
+    public int Reward { get; set; }
+
+    public string Id
+    {
+        get => User.Id.ToString();
+        set { }
+    }
+    public int Score
+    {
+        get => Success.Count;
+        set { }
+    }
+    public List<Team> Success => Picked?.Where(s => Fixed?.Contains(s) ?? false).ToList() ?? new();
+    public List<Team> Fail => Picked?.Where(s => !(Fixed?.Contains(s) ?? false)).ToList() ?? new();
+}
