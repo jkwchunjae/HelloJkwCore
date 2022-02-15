3ba64de8a3e05715c31085766cb48b674664b35c
jkw <jkwchunjae@daum.net>
2022-02-15 23:45:02 +0900

    2018 결과 파일 경로

--- HelloJkwCore/ProjectWorldCup/WorldCupOption.cs
+++ HelloJkwCore/ProjectWorldCup/WorldCupOption.cs
@@ -3,5 +3,6 @@
 public class WorldCupOption
 {
     public FileSystemSelectOption FileSystemSelect { get; set; }
+    public FileSystemSelectOption FileSystemSelect2018 { get; set; }
     public PathMap Path { get; set; }
 }
