6707cd5664ba9face551fb7c948f1720c3172507
jkw <jkwchunjae@daum.net>
2022-02-15 23:46:16 +0900

    2018 결과 interface

---
+++ HelloJkwCore/ProjectWorldCup/WorldCupService2018.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Caching;
+
+namespace ProjectWorldCup;
+
+public partial class WorldCupService
+{
+    public Task<List<WcBettingItem>> Get2018GroupStageBettingResult()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<WcBettingItem>> Get2018Round16BettingResult()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<WcFinalBettingItem>> Get2018FinalBettingResult()
+    {
+        throw new NotImplementedException();
+    }
+}
