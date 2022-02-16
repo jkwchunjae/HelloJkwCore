fc8420c47469ce0f7d57bc3e6cc10212cd7e7139
jkw <jkwchunjae@daum.net>
2022-02-17 00:18:24 +0900

    2018 결승전 결과 페이지

---
+++ HelloJkwCore/ProjectWorldCup/Pages/Betting/BettingFinalResult2018Component.razor.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectWorldCup.Pages.Betting;
+
+public partial class BettingFinalResult2018Component : JkwPageBase
+{
+    [Parameter]
+    public List<WcFinalBettingItem> BettingItems { get; set; }
+
+    BettingResultTable<WcFinalBettingItem> BettingResult { get; set; }
+
+    WcFinalBettingItem FirstItem => BettingResult?.FirstOrDefault();
+
+    public BettingFinalResult2018Component()
+    {
+        BettingResult = new BettingResultTable<WcFinalBettingItem>(new List<WcFinalBettingItem>());
+    }
+
+    protected override void OnPageParametersSet()
+    {
+        BettingResult = new BettingResultTable<WcFinalBettingItem>(BettingItems, new BettingTableOption
+        {
+            RewardForUser = reward => (reward / 10) * 10,
+        });
+    }
+}
