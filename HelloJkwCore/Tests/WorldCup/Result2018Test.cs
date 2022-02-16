853f4b42fbe133da3477351011ebd406330d75d8
jkw <jkwchunjae@daum.net>
2022-02-16 23:26:55 +0900

    2018 결승전 테스트

--- HelloJkwCore/Tests/WorldCup/Result2018Test.cs
+++ HelloJkwCore/Tests/WorldCup/Result2018Test.cs
@@ -80,4 +80,48 @@ public class Result2018Test
         Assert.Equal(11170, result.First(x => x.User.UserName == "seojisu205@gmail.com").Reward);
         Assert.Equal(10310, result.First(x => x.User.UserName == "taeheeyu@gmail.com").Reward);
     }
+
+    [Fact]
+    public async Task CalcFinalResultTest()
+    {
+        var list = await _worldCupService.Get2018FinalBettingResult();
+
+        var result = new BettingResultTable<WcFinalBettingItem>(list, new BettingTableOption
+        {
+            RewardForUser = reward =>
+            {
+                return (reward / 10) * 10; // 1의 자리 내림
+            },
+        });
+
+
+        Assert.Equal(26, list.Count);
+
+        Assert.Equal(470, result.First(x => x.User.UserName == "jkwchunjae@gmail.com").Reward);
+        Assert.Equal(1410, result.First(x => x.User.UserName == "ginasuace@gmail.com").Reward);
+        Assert.Equal(18430, result.First(x => x.User.UserName == "janfirsy@gmail.com").Reward);
+        Assert.Equal(1410, result.First(x => x.User.UserName == "skatpgusskat@gmail.com").Reward);
+        Assert.Equal(26000, result.First(x => x.User.UserName == "kyh9052@gmail.com").Reward);
+        Assert.Equal(24580, result.First(x => x.User.UserName == "haesolwon@gmail.com").Reward);
+        Assert.Equal(2830, result.First(x => x.User.UserName == "dsjun12@gmail.com").Reward);
+        Assert.Equal(18900, result.First(x => x.User.UserName == "seojisu205@gmail.com").Reward);
+        Assert.Equal(20800, result.First(x => x.User.UserName == "tmdgus7960@gmail.com").Reward);
+        Assert.Equal(18900, result.First(x => x.User.UserName == "sunin1206@gmail.com").Reward);
+        Assert.Equal(3300, result.First(x => x.User.UserName == "solientcg@gmail.com").Reward);
+        Assert.Equal(7090, result.First(x => x.User.UserName == "utaeyeon@gmail.com").Reward);
+        Assert.Equal(1410, result.First(x => x.User.UserName == "sikinmettugi@gmail.com").Reward);
+        Assert.Equal(940, result.First(x => x.User.UserName == "301386@gmail.com").Reward);
+        Assert.Equal(18430, result.First(x => x.User.UserName == "khs500kr@gmail.com").Reward);
+        Assert.Equal(940, result.First(x => x.User.UserName == "care3728@gmail.com").Reward);
+        Assert.Equal(1410, result.First(x => x.User.UserName == "axaxax11222@gmail.com").Reward);
+        Assert.Equal(18430, result.First(x => x.User.UserName == "mooming.go@gmail.com").Reward);
+        Assert.Equal(8980, result.First(x => x.User.UserName == "trouvailley@gmail.com").Reward);
+        Assert.Equal(20800, result.First(x => x.User.UserName == "flckddyd1@gmail.com").Reward);
+        Assert.Equal(20800, result.First(x => x.User.UserName == "gpdiadora3@gmail.com").Reward);
+        Assert.Equal(17960, result.First(x => x.User.UserName == "junghoonkim").Reward);
+        Assert.Equal(3300, result.First(x => x.User.UserName == "breaknow@bluehole.net").Reward);
+        Assert.Equal(940, result.First(x => x.User.UserName == "ldh1450@gmail.com").Reward);
+        Assert.Equal(470, result.First(x => x.User.UserName == "hinolja83@gmail.com").Reward);
+        Assert.Equal(940, result.First(x => x.User.UserName == "lucia").Reward);
+    }
 }
