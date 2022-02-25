d02a8770c8024fb4a8cc0ecaa5acead1bb526a47
jkw <jkwchunjae@daum.net>
2022-02-25 12:43:48 +0900

    Text component 생성

---
+++ HelloJkwCore/ProjectDiary/Pages/DiaryText.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjectDiary.Pages;
+
+public partial class DiaryText : JkwPageBase
+{
+    [Parameter]
+    public string Text { get; set; }
+
+    [Parameter]
+    public EventCallback<string> TextChanged { get; set; }
+
+    [Parameter]
+    public bool AutoFocus { get; set; } = false;
+
+
+    private async Task OnTextChanged(string text)
+    {
+        Text = text;
+
+        await TextChanged.InvokeAsync(text);
+    }
+}
