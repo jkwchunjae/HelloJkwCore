c2dffab4758870ea6b41003f2f36469b1d846cda
Kyungwon Jeon <jkwchunjae@daum.net>
2022-05-15 00:35:46 +0900

    맴버 추가

---
+++ HelloJkwCore/ProjectSuFc/Pages/SuFcMembers.razor.cs
@@ -0,0 +1,37 @@
+﻿namespace ProjectSuFc.Pages;
+
+public partial class SuFcMembers : JkwPageBase
+{
+    [Inject] ISuFcService Service { get; set; }
+
+    List<Member> Members { get; set; } = new();
+
+    string NewMemberName = string.Empty;
+
+    protected override async Task OnPageInitializedAsync()
+    {
+        Members = await Service.GetAllMember();
+    }
+
+    private async Task AddNewMember(string memberNameText)
+    {
+        if (string.IsNullOrWhiteSpace(memberNameText))
+            return;
+
+        if (memberNameText.Contains(' '))
+            return;
+
+        if (Path.GetInvalidFileNameChars().Any(chr => memberNameText.Contains(chr)))
+            return;
+
+        var memberName = new MemberName(memberNameText);
+
+        await Service.SaveMember(new Member()
+        {
+            Name = memberName,
+        });
+
+        NewMemberName = string.Empty;
+        Members = await Service.GetAllMember();
+    }
+}
