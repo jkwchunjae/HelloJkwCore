9adc5cfccf82b065d245e53cfe01a0dc22fc6c49
Kyungwon Jeon <jkwchunjae@daum.net>
2022-06-12 17:51:42 +0900

    내가 읽을 수 있는 일기장 목록 표시

---
+++ HelloJkwCore/ProjectDiary/Pages/DiaryViewableList.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectDiary.Pages;
+
+public partial class DiaryViewableList : JkwPageBase
+{
+    [Inject] private IDiaryService DiaryService { get; set; }
+
+    private UserDiaryInfo UserDiaryInfo;
+
+    protected override async Task OnPageInitializedAsync()
+    {
+        if (IsAuthenticated)
+        {
+            UserDiaryInfo = await DiaryService.GetUserDiaryInfoAsync(User);
+        }
+    }
+}
