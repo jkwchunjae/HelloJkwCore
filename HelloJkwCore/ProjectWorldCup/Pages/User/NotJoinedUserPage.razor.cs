298cd18d4f937c4cbd3b23fc81b03206ea507634
Kyungwon Jeon <jkwchunjae@daum.net>
2022-08-31 02:53:17 +0900

    유저 입장 첫 페이지

---
+++ HelloJkwCore/ProjectWorldCup/Pages/User/NotJoinedUserPage.razor.cs
@@ -0,0 +1,29 @@
+﻿namespace ProjectWorldCup.Pages.User;
+
+public partial class NotJoinedUserPage : JkwPageBase
+{
+    [Inject] IBettingService Service { get; set; }
+
+    BettingUser BettingUser;
+    protected override async Task OnPageInitializedAsync()
+    {
+        if (IsAuthenticated)
+        {
+            BettingUser = await Service.GetBettingUserAsync(User);
+
+            if (BettingUser?.JoinStatus == UserJoinStatus.Joined)
+            {
+                Navi.NavigateTo(""); // TODO: 가입 한 사람이 가야할 곳으로 안내
+            }
+        }
+    }
+
+    private async Task JoinAsync()
+    {
+        if (IsAuthenticated)
+        {
+            BettingUser = await Service.MakeJoinRequestAsync(User);
+            StateHasChanged();
+        }
+    }
+}
