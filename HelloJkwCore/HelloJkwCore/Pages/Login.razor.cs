039b1e7a85c68ca787d89128b81e50e2d2979878
Kyungwon Jeon <jkwchunjae@daum.net>
2022-11-06 01:49:15 +0900

    구글 로그인은 크롬에서만 가능하다

--- HelloJkwCore/HelloJkwCore/Pages/Login.razor.cs
+++ HelloJkwCore/HelloJkwCore/Pages/Login.razor.cs
@@ -1,5 +1,30 @@
-﻿namespace HelloJkwCore.Pages;
+﻿using MudBlazor;
+
+namespace HelloJkwCore.Pages;
 
 public partial class Login : ComponentBase
 {
+    [Inject] public IJSRuntime Js { get; set; }
+    [Inject] public ISnackbar Snackbar { get; set; }
+
+    private bool IsChromeBrowser => ExceptApps.Any(app => UserAgent.Contains(app)) ? false : true;
+    private string[] ExceptApps = new[] { "kakao", "naver" };
+    private string UserAgent = string.Empty;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            var userAgent = await Js.InvokeAsync<string>("getUserAgent");
+            await Js.InvokeVoidAsync("console.log", "UserAgent", userAgent);
+            UserAgent = userAgent.ToLower();
+            StateHasChanged();
+        }
+    }
+
+    private void LoginGoogleInOtherBrowser()
+    {
+        Snackbar.Configuration.PositionClass = Defaults.Classes.Position.TopCenter;
+        Snackbar.Add("구글 로그인은 크롬에서 해주세요.");
+    }
 }
