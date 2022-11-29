a60e78f983617c2ea678a520188f8d90e5685bce
Kyungwon Jeon <jkwchunjae@daum.net>
2022-11-30 06:23:11 +0900

    앞 경기의 결과를 가지고 반영해보자. 승부차기는 어떻게 될지 모르겠다

--- HelloJkwCore/ProjectWorldCup/WorldCupService.cs
+++ HelloJkwCore/ProjectWorldCup/WorldCupService.cs
@@ -26,30 +26,33 @@ public partial class WorldCupService : IWorldCupService
     public async Task<List<KnMatch>> GetRound16MatchesAsync()
     {
         var matches = await _fifa.GetRound16MatchesAsync();
+        var groupStanding = await _fifa.GetStandingDataAsync();
 
         return matches
             .Where(match => match.IdStage == Fifa.Round16StageId)
-            .Select(match => KnMatch.CreateFromFifaMatchData(match))
+            .Select(match => KnMatch.CreateFromFifaMatchData(match, groupStanding))
             .ToList();
     }
 
     public async Task<List<KnMatch>> GetQuarterFinalMatchesAsync()
     {
+        var match16 = await _fifa.GetRound16MatchesAsync();
         var matches = await _fifa.GetFinalMatchesAsync();
 
         return matches
             .Where(match => match.IdStage == Fifa.Round8StageId)
-            .Select(match => KnMatch.CreateFromFifaMatchData(match))
+            .Select(match => KnMatch.CreateFromFifaMatchData(match, match16))
             .ToList();
     }
 
     public async Task<List<KnMatch>> GetFinalMatchesAsync()
     {
+        var match16 = await _fifa.GetRound16MatchesAsync();
         var matches = await _fifa.GetFinalMatchesAsync();
 
         return matches
             .Where(match => match.IdStage != Fifa.Round16StageId)
-            .Select(match => KnMatch.CreateFromFifaMatchData(match))
+            .Select(match => KnMatch.CreateFromFifaMatchData(match, match16))
             .ToList();
     }
 }
