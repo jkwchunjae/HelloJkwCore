b24be80cd65dcb62ebad5ea3f481bf586f90ce03
Kyungwon Jeon <jkwchunjae@daum.net>
2023-01-21 18:07:48 +0900

    월드컵 프로젝트 정리 (#75)
    * 홈페이지에서 월드컵 관련 링크 제거
    
    * 월드컵 테스트 프로젝트 분리 (폴더 위치도 변경)
    
    * 솔루션에서 월드컵 프로젝트 삭제

--- HelloJkwCore/HelloJkwCore/Startup.cs
+++ HelloJkwCore/HelloJkwCore/Startup.cs
@@ -143,7 +143,6 @@ public class Startup
         services.AddDiaryService(Configuration);
         services.AddSuFcService(Configuration);
         services.AddBadukService(Configuration);
-        services.AddWorldCupService(Configuration);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ HelloJkwCore/Tests/Tests.WorldCup/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Tests.WorldCup;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
