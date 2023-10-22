45b2aa00b45a08679709e4e76e1502886e78d6e9
Kyungwon Jeon <jkwchunjae@daum.net>
2023-10-22 09:59:16 +0900

    테스트 입력 창

--- HelloJkwCore/GameLibra/Page/LibraAssistComponent.razor.cs
+++ HelloJkwCore/GameLibra/Page/LibraAssistComponent.razor.cs
@@ -4,6 +4,7 @@ namespace GameLibra.Page;
 
 public partial class LibraAssistComponent : JkwPageBase
 {
+    [Parameter] public LibraGameState State { get; set; }
     [Parameter] public IReadOnlyList<IReadOnlyDictionary<string, int>> AssistSets { get; set; }
     IReadOnlyList<IReadOnlyDictionary<string, int>> Sets
     {
@@ -24,8 +25,39 @@ public partial class LibraAssistComponent : JkwPageBase
         .Select(x => x.Key)
         .ToList();
 
+    string calcFormula = string.Empty;
+    Dictionary<IReadOnlyDictionary<string, int>, object> calcResult = new();
+
     protected override Task OnPageInitializedAsync()
     {
         return base.OnPageInitializedAsync();
+    }
+
+    private Task FormulaChagned(string formula)
+    {
+        // formula: abc -> int
+        // formula: a+b+c -> int
+        // formula: ab < ee -> boolean
+        // formula: aa = bb -> boolean
+        calcResult = new();
+        calcFormula = formula;
+
+        if (string.IsNullOrWhiteSpace(formula))
+        {
+            return Task.CompletedTask;
+        }
+
+        if (!new CubeCalculator().TestValidFormula(formula, CubeNames))
+        {
+            return Task.CompletedTask;
+        }
+
+        Sets?.ForEach(set =>
+        {
+            var result = new CubeCalculator().CalculateFormula(formula, set);
+            calcResult[set] = result;
+        });
+
+        return Task.CompletedTask;
     }
 }
