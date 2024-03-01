5b8371cbe3366ae193547f58d2c2f36c0d5c2ac9
Kyungwon Jeon <jkwchunjae@daum.net>
2024-03-02 02:49:38 +0900

    Auth (1/N) backend build !

---
+++ HelloJkwCore/HelloJkwCore2/Authentication/AuthProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloJkwCore2.Authentication;
+
+[TextJsonConverter(typeof(TextJsonStringEnumConverter))]
+public enum AuthProvider
+{
+    Google,
+    KakaoTalk,
+    Dropbox,
+}
--- HelloJkwCore/HelloJkwCore2/Program.cs
+++ HelloJkwCore/HelloJkwCore2/Program.cs
@@ -1,13 +1,61 @@
+using HelloJkwCore2.Authentication;
 using HelloJkwCore2.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
 using MudBlazor.Services;
+using HelloJkwCore2;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var coreOption = CoreOption.Create(builder.Configuration);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddHostedService<QueuedHostedService>();
+builder.Services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
+builder.Services.AddHttpClient();
 
 builder.Services.AddMudServices();
+
+#region FileSystem
+
+var fsOption = new FileSystemOption();
+builder.Configuration.GetSection("FileSystem").Bind(fsOption);
+builder.Services.AddSingleton(fsOption);
+builder.Services.AddSingleton<IFileSystemService, FileSystemService>();
+
+#endregion
+
+#region Authentication
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+AuthUtil authUtil = AuthUtil.Create(builder.Configuration, coreOption);
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddGoogleAuthentication(authUtil)
+    .AddKakaoAuthentication(authUtil)
+    .AddIdentityCookies();
+
+builder.Services
+    .AddIdentityCore<ApplicationUser>()
+    .AddUserManager<AppUserManager>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IUserStore<ApplicationUser>, AppUserStore>();
+builder.Services.AddSingleton<IRoleStore<ApplicationRole>, AppRoleStore>();
+
+#endregion
 
 var app = builder.Build();
 
@@ -26,5 +74,8 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
 
 app.Run();
