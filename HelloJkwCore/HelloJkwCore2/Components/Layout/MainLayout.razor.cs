67b7053243670277618ba6b4867bc05a648d5e78
Kyungwon Jeon <jkwchunjae@daum.net>
2024-03-02 01:36:31 +0900

    I love MudBlazor

---
+++ HelloJkwCore/HelloJkwCore2/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,41 @@
+﻿using Common;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace HelloJkwCore2.Components.Layout;
+
+public partial class MainLayout : LayoutComponentBase
+{
+    bool _drawerOpen = true;
+    MudTheme currentTheme = ThemeFamily.GetTheme(ThemeType.Default);
+    ThemeType _currentThemeType = ThemeType.Default;
+
+    protected override Task OnInitializedAsync()
+    {
+        _currentThemeType = ThemeType.Default;
+        //if (IsAuthenticated)
+        //{
+        //    _currentThemeType = User.Theme;
+        //    currentTheme = ThemeFamily.GetTheme(User.Theme);
+        //}
+
+        return Task.CompletedTask;
+    }
+
+    void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+
+    void ToggleTheme()
+    {
+        _currentThemeType = ThemeFamily.Next(_currentThemeType);
+        currentTheme = ThemeFamily.GetTheme(_currentThemeType);
+
+        //if (IsAuthenticated)
+        //{
+        //    User.Theme = _currentThemeType;
+        //    UserStore.UpdateAsync(User, CancellationToken.None);
+        //}
+    }
+}
