2506e7f8964aa50ed3752d7db43e4dbd2b0f21d5
Jeon Kyung-won <jkwchunjae@daum.net>
2024-04-11 00:35:04 +0900

    내가 쓸 수 있는 일기장 표시 (#102)
    * 내가 쓸 수 있는 일기장 표시
    
    * 내 일기장 공개설정을 변경할 수 있다.
    
    * 하정이 육아일기장을 위한 하드코딩

---
+++ HelloJkwCore/ProjectDiary/Pages/DiaryWritableList.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectDiary.Pages;
+
+public partial class DiaryWritableList : JkwPageBase
+{
+    [Inject] private IDiaryService DiaryService { get; set; }
+
+    private UserDiaryInfo UserDiaryInfo;
+
+    protected override async Task OnPageInitializedAsync()
+    {
+        if (IsAuthenticated)
+        {
+            UserDiaryInfo = await DiaryService.GetUserDiaryInfoAsync(User);
+        }
+    }
+}
