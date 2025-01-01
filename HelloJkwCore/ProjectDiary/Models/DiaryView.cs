a0edb705662aa49ec16d5be86db1cb8680e1e4cb
Kyungwon Jeon <jkwchunjae@daum.net>
2025-01-01 11:19:57 +0900

    DiaryView.PictureUrl

--- HelloJkwCore/ProjectDiary/Models/DiaryView.cs
+++ HelloJkwCore/ProjectDiary/Models/DiaryView.cs
@@ -4,6 +4,7 @@ public class DiaryView
 {
     public DiaryInfo DiaryInfo { get; set; }
     public List<DiaryContent> DiaryContents { get; set; }
+    public List<string> PicturesUrl { get; set; }
     public List<string> PicturesBase64 { get; set; }
     public DiaryNavigationData DiaryNavigationData { get; set; }
 }
