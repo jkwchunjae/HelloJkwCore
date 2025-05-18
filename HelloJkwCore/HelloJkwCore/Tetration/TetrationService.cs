cd5e852226a577ba82ec3aad87c8d0dc450338f3
Kyungwon Jeon <jkwchunjae@daum.net>
2025-05-18 15:17:27 +0900

    Chunked

--- HelloJkwCore/HelloJkwCore/Tetration/TetrationService.cs
+++ HelloJkwCore/HelloJkwCore/Tetration/TetrationService.cs
@@ -12,6 +12,7 @@ public record struct TeOptions(int MaxIterations, double DivergenceRadius, doubl
 public record struct TetrationResult(string Base64Image, TePoint Center, TeSize Size, TeOptions Options);
 public class TetrationService
 {
+    public event EventHandler<TetrationResult>? OnTetrationResult;
     public async Task<TetrationResult> CreateTetrationImage(TePoint center, TeSize size, TeOptions options)
     {
         var divergenceMap = GetTetrationDivergedTable(center, size, options);
@@ -25,6 +26,19 @@ public class TetrationService
         var divergenceMap = GetTetrationDivergedTable(rectangle, imageSize, options);
         var imagePath = @$"./my-tetration/image-{Guid.NewGuid()}.png";
         await SaveBoolArrayAsImage(divergenceMap, imagePath);
+        var base64Image = await File.ReadAllBytesAsync(imagePath);
+        return new TetrationResult(Convert.ToBase64String(base64Image), default, default, options);
+    }
+    public async Task<TetrationResult> CreateTetrationImageChunk(TeRactangle rectangle, TeSize imageSize, TeOptions options)
+    {
+        var imagePath = @$"./my-tetration/image-{Guid.NewGuid()}.png";
+        var divergenceMap = await GetTetrationDivergedTable(rectangle, imageSize, options, async (map) =>
+        {
+            await SaveBoolArrayAsImage(map, imagePath);
+            var base64Image = await File.ReadAllBytesAsync(imagePath);
+            var result = new TetrationResult(Convert.ToBase64String(base64Image), default, default, options);
+            OnTetrationResult?.Invoke(this, result);
+        });
         var base64Image = await File.ReadAllBytesAsync(imagePath);
         return new TetrationResult(Convert.ToBase64String(base64Image), default, default, options);
     }
@@ -60,6 +74,40 @@ public class TetrationService
             {
                 divergenceMap[x.Column, x.Row] = x.Diverged;
             });
+
+        return divergenceMap;
+    }
+    public async Task<bool[,]> GetTetrationDivergedTable(TeRactangle rectangle, TeSize imageSize, TeOptions options, Func<bool[,], Task> onProgress)
+    {
+        bool[,] divergenceMap = new bool[imageSize.Width, imageSize.Height];
+
+        var chunked = Enumerable.Range(0, imageSize.Width)
+            .Join(Enumerable.Range(0, imageSize.Height), i => true, j => true, (i, j) => (Column: i, Row: j))
+            .Chunk(imageSize.Width * imageSize.Height / 10);
+
+        foreach (var chunk in chunked)
+        {
+            chunk
+                .AsParallel()
+                .Select(x => new
+                {
+                    x.Column,
+                    x.Row,
+                    real = x.Column / (double)imageSize.Width * (rectangle.RightBottom.X - rectangle.LeftTop.X) + rectangle.LeftTop.X,
+                    imag = x.Row / (double)imageSize.Height * (rectangle.RightBottom.Y - rectangle.LeftTop.Y) + rectangle.LeftTop.Y,
+                    //     (   0   -  5  ) + 2 * 5 * i   
+                    // real = center.X - epsX + 2.0 * epsX * x.i / size.Width,
+                    // imag = center.Y - epsY + 2.0 * epsY * x.j / size.Height,
+                })
+                .Select(x => new { x.Column, x.Row, C = new Complex(x.real, x.imag) })
+                .Select(x => new { x.Column, x.Row, Diverged = TetrationDivergedTest(x.C, options.MaxIterations, options.DivergenceRadius) })
+                .ToList()
+                .ForEach(x =>
+                {
+                    divergenceMap[x.Column, x.Row] = x.Diverged;
+                });
+            await onProgress.Invoke(divergenceMap);
+        }
 
         return divergenceMap;
     }
