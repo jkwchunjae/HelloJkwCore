9a0ec14e659ef2448b64a9092bc9f7f05c97361d
Kyungwon Jeon <jkwchunjae@daum.net>
2025-10-23 23:33:57 +0900

    window.keyListner 별도 파일로 분리

---
+++ HelloJkwCore/HelloJkwCore/wwwroot/js/keyListener.js
@@ -0,0 +1,27 @@
+﻿
+var handlers = {};
+
+window.keyListener = {
+    initialize: function (dotnetHelper) {
+        var keydownHandler = function (e) {
+            dotnetHelper.invokeMethodAsync('OnKeyDown', e.key);
+        };
+        var keyupHandler = function (e) {
+            dotnetHelper.invokeMethodAsync('OnKeyUp', e.key);
+        };
+        document.addEventListener('keydown', keydownHandler);
+        document.addEventListener('keyup', keyupHandler);
+        handlers[dotnetHelper] = {
+            keydown: keydownHandler,
+            keyup: keyupHandler,
+        };
+    },
+    dispose: function (dotnetHelper) {
+        var handler = handlers[dotnetHelper];
+        if (handler) {
+            document.removeEventListener('keydown', handler.keydown);
+            document.removeEventListener('keyup', handler.keyup);
+            delete handlers[dotnetHelper];
+        }
+    }
+};
