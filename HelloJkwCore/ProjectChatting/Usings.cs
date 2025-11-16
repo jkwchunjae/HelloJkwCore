02e1563d200647c7d140618f9b8f0c7edc1221d2
Kyungwon Jeon <jkwchunjae@daum.net>
2025-11-16 22:56:36 +0900

    ProjectChatting.csproj

---
+++ HelloJkwCore/ProjectChatting/Usings.cs
@@ -0,0 +1,19 @@
+﻿global using JkwExtensions;
+global using Common;
+global using Microsoft.AspNetCore.Components;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.IO;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.AspNetCore.Components.Routing;
+global using Microsoft.JSInterop;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using TextJsonConverter = System.Text.Json.Serialization.JsonConverterAttribute;
+global using TextJsonIgnore = System.Text.Json.Serialization.JsonIgnoreAttribute;
+global using TextJsonStringEnumConverter = System.Text.Json.Serialization.JsonStringEnumConverter;
+
+namespace ProjectChatting;
