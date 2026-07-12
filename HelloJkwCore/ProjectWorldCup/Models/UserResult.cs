efebeec51d847ffc5cf8d4252a4e2591ac605d85
Kyungwon Jeon <jkwchunjae@daum.net>
2026-07-13 00:38:00 +0900

    요약 정보에 불참을 표시

--- HelloJkwCore/ProjectWorldCup/Models/UserResult.cs
+++ HelloJkwCore/ProjectWorldCup/Models/UserResult.cs
@@ -6,10 +6,10 @@ public class UserResult
     public string Name { get; set; }
     public UserId UserId { get; set; }
     public int JoinCount { get; set; }
-    public long Reward1 { get; set; }
-    public long Reward2 { get; set; }
-    public long Reward3 { get; set; }
-    public long Reward4 { get; set; }
-    public long Total => Reward1 + Reward2 + Reward3 + Reward4;
+    public long? Reward1 { get; set; }
+    public long? Reward2 { get; set; }
+    public long? Reward3 { get; set; }
+    public long? Reward4 { get; set; }
+    public long Total => (Reward1 ?? 0) + (Reward2 ?? 0) + (Reward3 ?? 0) + (Reward4 ?? 0);
     public long Profit => 0 - JoinCount * 10000 + Total;
 }
